1bed6a1879ecfbe7b115a11d9c36ade679099194
Alex Wenstrup <awenstrup@olin.edu>
2020-12-03 19:27:21 -0500

    Get hamming encoder working in standard form

--- main.py
+++ main.py
@@ -1,48 +1,125 @@
-from random import random
+# Imports
 
-def add_noise(s: str, bitrate: float) -> str:
-    """Given a bit-string, flip ROUGHLY (bitrate*100) % of the bits
+import numpy as np
 
-    Args:
-        s: The input string
-        pct: The percentage change of each bit being flipped
+# Constants
+# G = 
+# H = np.linalg.solve(G, np.zeros(7, 7), dtype=int)
+# H_t = np.transpose(H)
 
-    Returns:
-        str: A noisy string
+G_standard = np.array([  # np.linalg.solve(H_t_standard, np.zeros((4, 3), dtype=int))
+    [0, 1, 1, 1, 0, 0, 0],
+    [1, 0, 1, 0, 1, 0, 0],
+    [1, 1, 0, 0, 0, 1, 0],
+    [1, 1, 1, 0, 0, 0, 1]
+])
+
+H_standard = np.array([
+    [1, 0, 0, 0, 1, 1, 1],
+    [0, 1, 0, 1, 0, 1, 1],
+    [0, 0, 1, 1, 1, 0, 1],
+])
+
+H_t_standard = np.transpose(H_standard)
+
+decode_standard = np.array([
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1],
+])
+
+standard_syndrome_lookup = {  # key:value = syndrome:bit_position
+    (0, 0, 0): 0,
+    (0, 0, 1): 3,
+    (0, 1, 0): 2,
+    (0, 1, 1): 4,
+    (1, 0, 0): 1,
+    (1, 0, 1): 5,
+    (1, 1, 0): 6,
+    (1, 1, 1): 7,
+}
+
+
+def hamming_encode_743(message: np.ndarray, standard: bool = False) -> np.ndarray:
+    """Given a 4 bit message, encode it as a 7 bit hamming code.
+
+    :param np.ndarray message: A 4 bit message to encode, as an ndarray of shape (4,)
+    :param bool standard: Use the standard form of G and H (defaults to ``False``)
+    
+    :rtype: np.ndarray
+    :returns: The encoded message, as an ndarray of shape (7,)
     """
-    out = ""
-    for c in s:
-        if not (c == "0" or c == "1"): 
-            raise Exception(f"Invalid input: {s}. Must be a bit string.")
+    if message.shape != (4,):
+        raise Exception(f"hamming_encode_743 expects a vector of size (4,), not {message.shape}")
 
-        if random() < bitrate:  # Flip the bit
-            out += ("0" if c == "1" else "0")
-        else:  # Don't flip the bit
-            out += c
+    g = G_standard if standard else G
 
-    return out
+    encoded = np.mod(message @ g, 2)  # matrix multiplication, mod 2
+    return encoded
 
-def hamming_encode(s: str, expected_bitrate: float, required_confidence: float) -> str:
-    """Given a bit-string s, encode it with a hamming code
+def hamming_decode_743(received: np.ndarray, standard: bool = False) -> np.ndarray:
+    """Given a 7 bit code, decode it to a 4 bit message.
 
-    TODO get rid of if/else; generalize
+    :param np.ndarray received: A 7 bit received code, as an ndarray of shape (7,)
+    :param bool standard: Use the standard form of G and H (defaults to ``False``)
+    
+    :rtype: np.ndarray
+    :returns: The decoded message, as an ndarray of shape (4,)
     """
-    alpha = 0  # Calculate based off of expected bitrate and required confidence
+    if received.shape != (7,):
+        raise Exception(f"hamming_decode_743 expects a vector of size (7,), not {message.shape}")
 
-    out = ""
-    if delta > 0.5:  # Use [3, 1, 3] hamming code TODO 0.5 isn't right
-        for c in s:
-            out += c*3
-    else:  # Use [7, 4, 3] hamming code
-        raise Exception("Not implemented")
+    h_t = H_t_standard if standard else H_t
 
-def hamming_decode(s):
-    """Given a bit-string s, assume it was hamming encoded, and hamming decode it
+    syndrome = np.mod(received @ h_t, 2)
+    print(f"syndrome: {syndrome}")
+    corrected = _correct_743(received=received, syndrome=syndrome, standard=standard)
+    print(f"fixed code word: {corrected}")
 
+    return _undo_encoding_743(corrected=corrected, standard=standard)
+
+def _correct_743(received: np.ndarray, syndrome: np.ndarray, standard: bool) -> np.ndarray:
+    """Given a 7 bit code and a syndrome, return the corrected 7 bit code.
+
+    :param np.ndarray received: A 7 bit received message of shape (7,)
+    :param np.ndarray syndrome: A 3 bit calculated syndrome of shape (3,)
+    :param bool standard: Use the standard form of G and H (defaults to ``False``)
+    
+    :rtype: np.ndarray
+    :returns: The corrected, encoded message, as an ndarray of shape (7,)
     """
-    if len(s) % 3 == 0:  # We used [3, 1, 3] encoding
-        raise Exception("Not implemented")
-    elif len(s) % 7 == 0:  # We used [7, 4, 3] encoding
-        raise Exception("Not implemented")
+    if standard:
+        position = standard_syndrome_lookup[tuple(syndrome)]
+        if position == 0:  # No error found
+            return received
+        else:
+            np.put(received, position - 1, (1 - received[position - 1]))
+            return received
+    else:
+        raise Exception("Not supported yet!")
 
-print(add_noise("11010", 20))
+def _undo_encoding_743(corrected: np.ndarray, standard: bool = False):
+    """Given a (fixed) codeword, undo its encoding back to a 4 bit array
+
+    :param np.ndarray codeword: A shape (7,) codeword
+
+    :rtype: np.ndarray
+    :returns: The decoded message
+    """
+    d = decode_standard if standard else decode
+    return np.mod(corrected @ d, 2)
+
+
+ex = np.array([0, 1, 1, 0])
+err = np.array([0, 0, 0, 0, 0, 1, 0])
+print(f"message: {ex}")
+encoded = hamming_encode_743(ex, standard=True)
+print(f"code word: {encoded}")
+message = np.mod(encoded + err, 2)
+print(f"noisy code word: {message}")
+decoded_message = hamming_decode_743(message, standard=True)
+print(f"decoded message: {decoded_message}")
